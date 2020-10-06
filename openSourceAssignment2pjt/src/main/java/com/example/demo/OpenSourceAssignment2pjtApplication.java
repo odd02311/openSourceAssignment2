f23002a259f15be0825fc8d404b2fb15419c22fc
Owner <Owner@DESKTOP-UHKKTE9>
2020-10-06 20:21:46 +0900

    @첫커밋

---
+++ openSourceAssignment2pjt/src/main/java/com/example/demo/OpenSourceAssignment2pjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceAssignment2pjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSourceAssignment2pjtApplication.class, args);
+	}
+
+}
